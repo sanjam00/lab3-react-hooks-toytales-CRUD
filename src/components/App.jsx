41cff596e7d7824185f748579fc7f028a7635ffd
Sanaya James <sanaya.j.james@gmail.com>
2025-12-06 22:03:12 -0700

    Fetched and rendered toy list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
+  const [toyList, setToyList] = useState([])
   const [showForm, setShowForm] = useState(false);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then(res => {
+        if (!res.ok) { throw new Error("failed to fetch data") }
+        return res.json()
+      })
+      .then(data => {
+        console.log(data)
+        setToyList(data)
+      })
+      .catch(error => console.log("Error while fetching data:", error))
+  }, [])
+
+  console.log("toylist in App.jsx:", toyList)
 
   return (
     <>
@@ -18,7 +34,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toyList={toyList} />
     </>
   );
 }
--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({ toyList, props }) {
+  console.log("toylist:", toyList)
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{toyList.map(toy => (
+      <ToyCard toy={toy} key={toy.id} />
+    ))}</div>
   );
 }
 
